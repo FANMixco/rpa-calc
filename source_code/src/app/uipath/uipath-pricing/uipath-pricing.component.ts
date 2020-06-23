8e75291cd4c97b2fde0d61cca2fd16c91353d079
Federico Navarrete <darklord.navarrete@gmail.com>
2020-06-23 14:43:27 +0200

    Moved to dynamic logic

--- source_code/src/app/uipath/uipath-pricing/uipath-pricing.component.ts
+++ source_code/src/app/uipath/uipath-pricing/uipath-pricing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorage } from 'src/app/classes/local-storage';
 
 @Component({
   selector: 'app-uipath-pricing',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UipathPricingComponent implements OnInit {
 
-  constructor() { }
+  uiPathResults:any = [];
+
+  constructor() {
+    const storage = new LocalStorage();
+    const availableCopy = JSON.parse(storage.getLocalStorageValue("availableCopy"));
+    const infoUiPath = availableCopy[1].UiPath.Prices;
+
+    const keys = Object.keys(infoUiPath);
+    for(let i = 0; i< keys.length; i++){
+        const key = keys[i];
+        let price = '';
+
+        const val = infoUiPath[key];
+
+        if (val.priceExtended) {
+          price = val.priceExtended;
+        } else {
+          price = val.price.toString();
+        }
+
+        this.uiPathResults.push([val.name, price]);
+    }
+  }
 
   ngOnInit(): void {
   }
-
 }
