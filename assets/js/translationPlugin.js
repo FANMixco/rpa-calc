414be22a022134f453ad21fb000ee355211b5b4b
Federico Navarrete <darklord.navarrete@gmail.com>
2020-06-24 10:02:26 +0200

    Added Website

---
+++ assets/js/translationPlugin.js
@@ -0,0 +1,3 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'es,nl,de,fr,it,en,zh-CN,zh-TW,pl,pt,ru,ja,ko,cs,iw,ro', autoDisplay: false, layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
